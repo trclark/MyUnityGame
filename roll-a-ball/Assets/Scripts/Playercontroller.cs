ade19a5eedd172e719f55bba1de1bd236d172a9b
trclark <trclark@ucsc.edu>
2018-11-05 00:45:09 -0800

    First commit

---
+++ roll-a-ball/Assets/Scripts/Playercontroller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontroller : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Fixedupdate is called before any physics calculations
+    void FixedUpdate() {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
